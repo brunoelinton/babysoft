acf90c94268733d80cf304c8c9532086307ef71a
brunoelinton <brunoelinton@outlook.com>
2022-11-27 15:07:01 -0300

    Backend: Project created

---
+++ backend/src/test/java/com/healthcare/babysoft/BabysoftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.healthcare.babysoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BabysoftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
